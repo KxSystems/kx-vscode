05df87b7dcd503ebc64bf4ec81e21d1704ec1d14
Cale Teeter <cale.teeter@microsoft.com>
2023-03-30 09:26:44 -0400

    Update to allow installation to continue after registration for license

--- src/commands/installTools.ts
+++ src/commands/installTools.ts
@@ -30,129 +30,133 @@ export async function installTools(): Promise<void> {
   });
 
   if (licenseTypeResult?.label === 'Acquire license') {
+    let licenseCancel;
     await openUrl(ext.kdbDownload);
     await window
       .showInformationMessage(
-        'After receiving the email with license, please restart vscode.',
-        'More info'
+        'After receiving the email with license, click continue to proceed.',
+        'Continue',
+        'Cancel'
       )
       .then(async res => {
-        if (res === 'More info') {
-          await openUrl('https://www.bing.com'); // TODO: update to real url
+        if (res === 'Cancel') {
+          licenseCancel = true;
         }
       });
-    return;
-  } else {
-    const licensePicks: QuickPickItem[] = [
-      {
-        label: 'Paste license string (base64 string from email)',
-        detail: 'Paste in the base64 encoded license string from the email ',
-      },
-      {
-        label: 'Select license file',
-        detail: 'Select a file saved with the license',
-      },
-    ];
-
-    const licenseResult: QuickPickItem | undefined = await window.showQuickPick(licensePicks, {
-      placeHolder: 'Select an option for license',
-    });
-
-    if (licenseResult === undefined) {
+    if (licenseCancel) {
       return;
-    } else if (licenseResult.label == 'Paste license string (base64 string from email)') {
-      const licenseInput: InputBoxOptions = {
-        prompt: 'Paste the base64 encoded license string',
-        placeHolder: 'encoded license',
-        ignoreFocusOut: true,
-      };
-
-      await window.showInputBox(licenseInput).then(async encodedLicense => {
-        if (encodedLicense !== undefined) {
-          file = [await convertBase64License(encodedLicense)];
-        }
-      });
-    } else {
-      file = await window.showOpenDialog({
-        canSelectFiles: true,
-        canSelectFolders: false,
-        canSelectMany: false,
-        openLabel: 'Select a license file',
-      });
     }
+  }
 
-    if (!file) {
-      throw new Error();
-    }
+  const licensePicks: QuickPickItem[] = [
+    {
+      label: 'Paste license string (base64 string from email)',
+      detail: 'Paste in the base64 encoded license string from the email ',
+    },
+    {
+      label: 'Select license file',
+      detail: 'Select a file saved with the license',
+    },
+  ];
 
-    window
-      .withProgress(
-        {
-          location: ProgressLocation.Notification,
-          title: 'Installation of Q',
-          cancellable: true,
-        },
-        async (progress, token) => {
-          token.onCancellationRequested(() => {
-            console.log('User cancelled the installation.');
-          });
+  const licenseResult: QuickPickItem | undefined = await window.showQuickPick(licensePicks, {
+    placeHolder: 'Select an option for license',
+  });
 
-          progress.report({ increment: 0 });
+  if (licenseResult === undefined) {
+    return;
+  } else if (licenseResult.label == 'Paste license string (base64 string from email)') {
+    const licenseInput: InputBoxOptions = {
+      prompt: 'Paste the base64 encoded license string',
+      placeHolder: 'encoded license',
+      ignoreFocusOut: true,
+    };
 
-          progress.report({ increment: 10, message: 'Checking for running process...' });
+    await window.showInputBox(licenseInput).then(async encodedLicense => {
+      if (encodedLicense !== undefined) {
+        file = [await convertBase64License(encodedLicense)];
+      }
+    });
+  } else {
+    file = await window.showOpenDialog({
+      canSelectFiles: true,
+      canSelectFolders: false,
+      canSelectMany: false,
+      openLabel: 'Select a license file',
+    });
+  }
+
+  if (!file) {
+    throw new Error();
+  }
+
+  window
+    .withProgress(
+      {
+        location: ProgressLocation.Notification,
+        title: 'Installation of Q',
+        cancellable: true,
+      },
+      async (progress, token) => {
+        token.onCancellationRequested(() => {
+          console.log('User cancelled the installation.');
+        });
+
+        progress.report({ increment: 0 });
+
+        progress.report({ increment: 10, message: 'Checking for running process...' });
+        await delay(1000);
+        const result = await findPid(5001);
+        if (Number.isNaN(result)) {
+          // move the license file
+          progress.report({ increment: 30, message: 'Moving license file...' });
           await delay(1000);
-          const result = await findPid(5001);
-          if (Number.isNaN(result)) {
-            // move the license file
-            progress.report({ increment: 30, message: 'Moving license file...' });
-            await delay(1000);
-            await ensureDir(ext.context.globalStorageUri.fsPath);
-            await copy(file![0].fsPath, join(ext.context.globalStorageUri.fsPath, ext.kdbLicName));
+          await ensureDir(ext.context.globalStorageUri.fsPath);
+          await copy(file![0].fsPath, join(ext.context.globalStorageUri.fsPath, ext.kdbLicName));
 
-            // get the bits for Q
-            progress.report({ increment: 50, message: 'Getting the binaries...' });
-            await delay(1000);
-            if (process.platform == 'win32') {
-              const gpath = join(ext.context.globalStorageUri.fsPath, 'w64.zip');
-              if (!(await existsSync(gpath))) {
-                const response = await fetch(ext.kdbUrl);
-                await writeFile(gpath, await response.buffer());
-              }
-              await extract(gpath, { dir: ext.context.globalStorageUri.fsPath });
-            } else if (process.platform == 'darwin') {
-              const gpath = join(ext.context.globalStorageUri.fsPath, 'm64.zip');
-              if (!(await existsSync(gpath))) {
-                const response = await fetch(ext.kdbUrl);
-                await writeFile(gpath, await response.buffer());
-              }
-              await extract(gpath, { dir: ext.context.globalStorageUri.fsPath });
-            } else {
-              throw new Error('OS not supports, only Windows and Mac are supported.');
+          // get the bits for Q
+          progress.report({ increment: 50, message: 'Getting the binaries...' });
+          await delay(1000);
+          if (process.platform == 'win32') {
+            const gpath = join(ext.context.globalStorageUri.fsPath, 'w64.zip');
+            if (!(await existsSync(gpath))) {
+              const response = await fetch(ext.kdbUrl);
+              await writeFile(gpath, await response.buffer());
             }
-
-            // add the env var for the process
-            progress.report({ increment: 70, message: 'Setting up environment...' });
-            await delay(1000);
-            env.QHOME = ext.context.globalStorageUri.fsPath;
-
-            progress.report({ increment: 100, message: 'Starting Q...' });
-            await delay(1000);
+            await extract(gpath, { dir: ext.context.globalStorageUri.fsPath });
+          } else if (process.platform == 'darwin') {
+            const gpath = join(ext.context.globalStorageUri.fsPath, 'm64.zip');
+            if (!(await existsSync(gpath))) {
+              const response = await fetch(ext.kdbUrl);
+              await writeFile(gpath, await response.buffer());
+            }
+            await extract(gpath, { dir: ext.context.globalStorageUri.fsPath });
+          } else {
+            throw new Error('OS not supports, only Windows and Mac are supported.');
           }
 
-          return new Promise<void>(resolve => {
-            resolve();
-          });
+          // add the env var for the process
+          progress.report({ increment: 70, message: 'Setting up environment...' });
+          await delay(1000);
+          env.QHOME = ext.context.globalStorageUri.fsPath;
+
+          progress.report({ increment: 100, message: 'Starting Q...' });
+          await delay(1000);
         }
-      )
-      .then(async () => {
-        window.showInformationMessage('Installation of Q runtime completed successfully!');
-        const workingDirectory = join(
-          ext.context.globalStorageUri.fsPath,
-          process.platform == 'win32' ? 'w64' : 'm64'
-        );
-        await executeCommand(workingDirectory, 'q', '-p', '5001');
-      });
-  }
+
+        return new Promise<void>(resolve => {
+          resolve();
+        });
+      }
+    )
+    .then(async () => {
+      window.showInformationMessage('Installation of Q runtime completed successfully!');
+      const workingDirectory = join(
+        ext.context.globalStorageUri.fsPath,
+        process.platform == 'win32' ? 'w64' : 'm64'
+      );
+      await executeCommand(workingDirectory, 'q', '-p', '5001');
+    });
 }
 
 export async function stopQ(): Promise<void> {
