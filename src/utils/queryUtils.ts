2ae357d83836fefd9f1d3090574ec4b03d15a74d
Niall Farrell <nfarrell@kx.com>
2023-06-23 16:58:11 +0100

    KXI-26709 Add context support

--- src/utils/queryUtils.ts
+++ src/utils/queryUtils.ts
@@ -1,3 +1,7 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { ext } from "../extensionVariables";
+
 export function sanitizeQuery(query: string): string {
   if (query[0] === "`") {
     query = query + " ";
@@ -8,7 +12,7 @@ export function sanitizeQuery(query: string): string {
 }
 
 export function queryWrapper(): string {
-  // TODO: introduce type of view, ex: console, table and define wrapper for each one
-  //default return for now: console wrapper
-  return '.Q.trp[{x:parse$[-11=type x;-3!x;x];x:eval$[0h=type x;_[;x]neg(";";(::))~(first;last)@\\:x;x];system"c ",-3!20 200|c:system "c";r:`result`type`keys`meta`data!(1b;0h;();();.Q.s x);system"c ",-3!c;r};;{\'(x,"\n\n","\n"sv 2 sublist"\n"vs .Q.sbt y)}]';
+  return readFileSync(
+    ext.context.asAbsolutePath(join("resources", "evaluate.q"))
+  ).toString();
 }
