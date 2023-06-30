e31e6a87827aaec23467409ecdea55faa444fbe8
Philip Carneiro <126578219+Philip-Carneiro-KX@users.noreply.github.com>
2023-06-30 13:31:06 +0100

    add esbuild missing

--- esbuild.js
+++ esbuild.js
@@ -1,4 +1,5 @@
 const { build } = require("esbuild");
+const { copy } = require("esbuild-plugin-copy");
 
 const baseConfig = {
   bundle: true,
@@ -15,10 +16,49 @@ const extensionConfig = {
   platform: "node",
 };
 
+const serverConfig = {
+  ...baseConfig,
+  outfile: "./out/server.js",
+  entryPoints: ["./server/src/server.ts"],
+  format: "cjs",
+  external: ["vscode"],
+  platform: "node",
+  plugins: [
+    copy({
+      resolveFrom: "cwd",
+      assets: {
+        from: ["node_modules/web-tree-sitter/tree-sitter.wasm"],
+        to: ["./out"],
+      },
+    }),
+  ],
+};
+
+const webviewConfig = {
+  ...baseConfig,
+  target: "es2020",
+  format: "esm",
+  entryPoints: ["./src/webview/main.ts"],
+  outfile: "./out/webview.js",
+  plugins: [
+    copy({
+      resolveFrom: "cwd",
+      assets: {
+        from: ["src/webview/styles/*.css"],
+        to: ["./out"],
+      },
+    }),
+  ],
+};
+
 (async () => {
   try {
     await build(extensionConfig);
     console.log("extension build complete");
+    await build(serverConfig);
+    console.log("server build complete");
+    await build(webviewConfig);
+    console.log("build complete");
   } catch (err) {
     process.stderr.write(err.stderr);
     process.exit(1);
