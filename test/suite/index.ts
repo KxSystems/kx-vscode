ad624766c6eea66c8a298a4fec2f714589b21740
Niall Farrell <nfarrell@kx.com>
2023-07-06 15:15:51 +0100

    Update based on Cale's sample

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -4,30 +4,10 @@ import * as NYC from "nyc";
 import * as path from "path";
 
 import * as baseConfig from "@istanbuljs/nyc-config-typescript";
-
-async function setupCoverage() {
-  const nyc = new NYC({
-    ...baseConfig,
-    cwd: path.join(__dirname, "..", "..", ".."),
-    include: ["out/**/*.js"],
-    exclude: ["**/test/**", ".vscode-test/**"],
-    reporter: ["text", "html"],
-    all: true,
-    instrument: true,
-    hookRequire: true,
-    hookRunInContext: true,
-    hookRunInThisContext: true,
-  });
-
-  await nyc.reset();
-  await nyc.wrap();
-
-  return nyc;
-}
+import "source-map-support/register";
+import "ts-node/register";
 
 export async function run(): Promise<void> {
-  // const nyc = process.env.COVERAGE ? setupCoverage() : null;
-  const nyc = await setupCoverage();
   const options: Mocha.MochaOptions = {
     ui: "bdd",
     color: true,
@@ -40,21 +20,59 @@ export async function run(): Promise<void> {
     },
   };
 
+  const nyc = new NYC({
+    ...baseConfig,
+    cwd: path.join(__dirname, "..", "..", ".."),
+    include: ["out/**/*.js"],
+    exclude: ["out/test/**"],
+    reporter: ["text-summary", "html"],
+    all: true,
+    silent: false,
+    instrument: true,
+    hookRequire: true,
+    hookRunInContext: true,
+    hookRunInThisContext: true,
+  });
+
+  await nyc.reset();
+  await nyc.wrap();
+
+  Object.keys(require.cache)
+    .filter((f) => nyc.exclude.shouldInstrument(f))
+    .forEach((m) => {
+      console.warn("Module loaded before NYC, invalidating", m);
+      delete require.cache[m];
+      require(m);
+    });
+
   const mocha = new Mocha(options);
   const testsRoot = path.resolve(__dirname, "..");
 
-  const files = await glob("**/**.test.js", { cwd: testsRoot });
+  const files = glob.sync("**/**.test.js", { cwd: testsRoot });
 
   files.forEach((f) => mocha.addFile(path.resolve(testsRoot, f)));
 
   const failures: number = await new Promise((resolve) => mocha.run(resolve));
 
-  if (nyc) {
-    await nyc.writeCoverageFile();
-    await nyc.report();
-  }
+  await nyc.writeCoverageFile();
+
+  console.log(await captureStdout(nyc.report.bind(nyc)));
 
   if (failures > 0) {
     throw new Error(`${failures} tests failed.`);
   }
+}
+
+async function captureStdout(fn) {
+  const w = process.stdout.write;
+  let buffer = "";
+
+  process.stdout.write = (s) => {
+    buffer = buffer + s;
+    return true;
+  };
+
+  await fn();
+  process.stdout.write = w;
+  return buffer;
 }
