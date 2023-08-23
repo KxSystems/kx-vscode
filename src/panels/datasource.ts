da8be5dab6e86799727a674d73c19dcb28acf933
Philip Carneiro <pcarneiro@kx.com>
2023-08-23 15:24:17 +0100

    fix warning msg

--- src/panels/datasource.ts
+++ src/panels/datasource.ts
@@ -294,9 +294,6 @@ export class DataSourcesPanel {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="default-src 'none'; style-src ${webview.cspSource}; font-src ${webview.cspSource}; img-src ${webview.cspSource} https:; script-src 'nonce-${nonce}';" />
         <link rel="stylesheet" href="${resetStyleUri}" />
         <link rel="stylesheet" href="${vscodeStyleUri}" />
         <link rel="stylesheet" href="${styleUri}" />
