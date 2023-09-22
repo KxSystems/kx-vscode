e7723573851e4ab13ab716a7ba735e4f8d07819e
Cale Teeter <cale.teeter@microsoft.com>
2023-09-22 00:06:31 -0400

    Updates to add services tests

--- test/suite/services.test.ts
+++ test/suite/services.test.ts
@@ -11,9 +11,18 @@
  * specific language governing permissions and limitations under the License.
  */
 
+import { before } from "mocha";
+import assert from "node:assert";
+import { TreeItemCollapsibleState } from "vscode";
+import { Insights } from "../../src/models/insights";
+import { Server } from "../../src/models/server";
 import * as dataSourceTreeProvider from "../../src/services/dataSourceTreeProvider";
 import * as codeFlowLogin from "../../src/services/kdbInsights/codeFlowLogin";
-import * as kdbTreeProvider from "../../src/services/kdbTreeProvider";
+import {
+  InsightsNode,
+  KdbNode,
+  KdbTreeProvider,
+} from "../../src/services/kdbTreeProvider";
 import * as terminalProvider from "../../src/services/terminalProvider";
 
 describe("dataSourceTreeProvider", () => {
@@ -23,9 +32,124 @@ describe("dataSourceTreeProvider", () => {
 });
 
 describe("kdbTreeProvider", () => {
-  //write tests for src/services/kdbTreeProvider.ts
-  //function to be deleted after write the tests
-  kdbTreeProvider;
+  let servers: Server;
+  let insights: Insights;
+  let kdbNode: KdbNode;
+  let insightNode: InsightsNode;
+
+  before(() => {
+    servers = {
+      testServer: {
+        serverAlias: "testServerAlias",
+        serverName: "testServerName",
+        serverPort: "5001",
+        tls: false,
+        auth: false,
+        managed: false,
+      },
+    };
+    insights = {
+      testInsight: {
+        alias: "testInsightsAlias",
+        server: "testInsightsName",
+        auth: false,
+      },
+    };
+    kdbNode = new KdbNode(
+      ["child1"],
+      "testElement",
+      servers["testServer"],
+      TreeItemCollapsibleState.None
+    );
+    insightNode = new InsightsNode(
+      ["child1"],
+      "testElement",
+      insights["testInsight"],
+      TreeItemCollapsibleState.None
+    );
+  });
+
+  it("Validate creation of KDB provider", () => {
+    const kdbProvider = new KdbTreeProvider(servers, insights);
+    assert.notStrictEqual(
+      kdbProvider,
+      undefined,
+      "KdbTreeProvider should be created."
+    );
+  });
+
+  it("Validate reload of KDB provider", () => {
+    const kdbProvider = new KdbTreeProvider(servers, insights);
+    kdbProvider.reload();
+    assert.notStrictEqual(
+      kdbProvider,
+      undefined,
+      "KdbTreeProvider should be created."
+    );
+  });
+
+  it("Validate refreshing KDB provider with KDB instance", () => {
+    const kdbProvider = new KdbTreeProvider(servers, insights);
+    servers["testServer2"] = {
+      serverAlias: "testServer2Alias",
+      serverName: "testServer2Name",
+      serverPort: "5001",
+      tls: false,
+      auth: false,
+      managed: false,
+    };
+    kdbProvider.refresh(servers);
+    assert.notStrictEqual(
+      kdbProvider,
+      undefined,
+      "KdbTreeProvider should be created."
+    );
+  });
+
+  it("Validate refreshing KDB provide with Insights instancer", () => {
+    const kdbProvider = new KdbTreeProvider(servers, insights);
+    insights["testInsight2"] = {
+      alias: "testInsights2Alias",
+      server: "testInsights2Name",
+      auth: false,
+    };
+    kdbProvider.refreshInsights(insights);
+    assert.notStrictEqual(
+      kdbProvider,
+      undefined,
+      "KdbTreeProvider should be created."
+    );
+  });
+
+  it("Should return the KdbNode tree item element", () => {
+    const kdbProvider = new KdbTreeProvider(servers, insights);
+    const element = kdbProvider.getTreeItem(kdbNode);
+    assert.strictEqual(
+      element.label,
+      kdbNode.label,
+      "Get kdb node element is incorrect"
+    );
+  });
+
+  it("Should return the Insights tree item element", () => {
+    const kdbProvider = new KdbTreeProvider(servers, insights);
+    const element = kdbProvider.getTreeItem(insightNode);
+    assert.strictEqual(
+      element.label,
+      insightNode.label,
+      "Get insights node element is incorrect"
+    );
+  });
+
+  it("Should return no children for the tree when serverList is empty", async () => {
+    const kdbProvider = new KdbTreeProvider({}, {});
+    const result = await kdbProvider.getChildren();
+    assert.notStrictEqual(
+      result,
+      undefined,
+      "Children should not be undefined"
+    );
+  });
 });
 
 describe("terminalProvider", () => {
