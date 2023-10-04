c0ca6c636424f3b2f5df82f064d074d7b0dfc6bd
Cale Teeter <cale.teeter@microsoft.com>
2023-10-04 18:38:26 -0400

    Update for adding unit tests for server object models

---
+++ test/suite/models.test.ts
@@ -0,0 +1,221 @@
+/*
+ * Copyright (c) 1998-2023 Kx Systems Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+import assert from "assert";
+import sinon from "sinon";
+import {
+  ServerObject,
+  loadDictionaries,
+  loadFunctions,
+  loadNamespaces,
+  loadTables,
+  loadVariables,
+} from "../../src/models/serverObject";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const so = require("../../src/commands/serverCommand");
+const vw = require("../../src/models/serverObject");
+
+describe("Models", () => {
+  it("Should return empty ServerObjects array when none are loaded", async () => {
+    sinon.stub(so, "loadServerObjects").resolves(undefined);
+    const result = await loadNamespaces("");
+    assert.strictEqual(result.length, 0, "Namespaces returned should be zero.");
+    sinon.restore();
+  });
+
+  it("Should return a single server object that ia a namespace", async () => {
+    const testObject: ServerObject[] = [
+      {
+        id: 1,
+        pid: 1,
+        name: "test",
+        fname: "test",
+        typeNum: 1,
+        namespace: ".",
+        context: {},
+        isNs: true,
+      },
+    ];
+    sinon.stub(so, "loadServerObjects").resolves(testObject);
+    const result = await loadNamespaces();
+    assert.strictEqual(
+      result[0],
+      testObject[0],
+      "Single server object that is a namespace should be returned."
+    );
+    sinon.restore();
+  });
+
+  it("Should return a single server object that ia a namespace", async () => {
+    const testObject: ServerObject[] = [
+      {
+        id: 1,
+        pid: 1,
+        name: "test",
+        fname: "test",
+        typeNum: 1,
+        namespace: ".",
+        context: {},
+        isNs: true,
+      },
+    ];
+    sinon.stub(so, "loadServerObjects").resolves(testObject);
+    const result = await loadNamespaces(".");
+    assert.strictEqual(
+      result[0],
+      testObject[0],
+      `Single server object that is a namespace should be returned: ${JSON.stringify(
+        result
+      )}`
+    );
+    sinon.restore();
+  });
+
+  it("Should return empty ServerObjects array when none are loaded", async () => {
+    sinon.stub(so, "loadServerObjects").resolves(undefined);
+    const result = await loadDictionaries("");
+    assert.strictEqual(
+      result.length,
+      0,
+      "ServerObjects returned should be zero."
+    );
+    sinon.restore();
+  });
+
+  it("Should return a single server object that ia a dictionary", async () => {
+    const testObject: ServerObject[] = [
+      {
+        id: 1,
+        pid: 1,
+        name: "test",
+        fname: "test",
+        typeNum: 99,
+        namespace: ".",
+        context: {},
+        isNs: false,
+      },
+    ];
+    sinon.stub(so, "loadServerObjects").resolves(testObject);
+    const result = await loadDictionaries(".");
+    assert.strictEqual(
+      result[0],
+      testObject[0],
+      "Single server object that is a namespace should be returned."
+    );
+    sinon.restore();
+  });
+
+  it("Should return empty ServerObjects array when none are loaded", async () => {
+    sinon.stub(so, "loadServerObjects").resolves(undefined);
+    const result = await loadFunctions(".");
+    assert.strictEqual(
+      result.length,
+      0,
+      "ServerObjects returned should be zero."
+    );
+    sinon.restore();
+  });
+
+  it("Should return a single server object that ia a function", async () => {
+    const testObject: ServerObject[] = [
+      {
+        id: 1,
+        pid: 1,
+        name: "test",
+        fname: "test",
+        typeNum: 100,
+        namespace: ".",
+        context: {},
+        isNs: false,
+      },
+    ];
+    sinon.stub(so, "loadServerObjects").resolves(testObject);
+    const result = await loadFunctions(".");
+    assert.strictEqual(
+      result[0],
+      testObject[0],
+      "Single server object that is a namespace should be returned."
+    );
+    sinon.restore();
+  });
+
+  it("Should return empty ServerObjects array when none are loaded", async () => {
+    sinon.stub(so, "loadServerObjects").resolves(undefined);
+    const result = await loadTables(".");
+    assert.strictEqual(
+      result.length,
+      0,
+      "ServerObjects returned should be zero."
+    );
+    sinon.restore();
+  });
+
+  it("Should return a single server object that ia a table", async () => {
+    const testObject: ServerObject[] = [
+      {
+        id: 1,
+        pid: 1,
+        name: "test",
+        fname: "test",
+        typeNum: 98,
+        namespace: ".",
+        context: {},
+        isNs: false,
+      },
+    ];
+    sinon.stub(so, "loadServerObjects").resolves(testObject);
+    const result = await loadTables(".");
+    assert.strictEqual(
+      result[0],
+      testObject[0],
+      "Single server object that is a namespace should be returned."
+    );
+    sinon.restore();
+  });
+
+  it("Should return empty ServerObjects array when none are loaded", async () => {
+    sinon.stub(so, "loadServerObjects").resolves(undefined);
+    const result = await loadVariables(".");
+    assert.strictEqual(
+      result.length,
+      0,
+      "ServerObjects returned should be zero."
+    );
+    sinon.restore();
+  });
+
+  it("Should return a single server object that ia a variable", async () => {
+    const testObject: ServerObject[] = [
+      {
+        id: 1,
+        pid: 1,
+        name: "test",
+        fname: "test",
+        typeNum: -7,
+        namespace: ".",
+        context: {},
+        isNs: false,
+      },
+    ];
+    sinon.stub(so, "loadServerObjects").resolves(testObject);
+    sinon.stub(vw, "loadViews").resolves([]);
+    const result = await loadVariables(".");
+    assert.strictEqual(
+      result[0],
+      testObject[0],
+      "Single server object that is a namespace should be returned."
+    );
+    sinon.restore();
+  });
+});
