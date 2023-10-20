6014bf85a90c17eeec2026a07d7fc35cd07e56d5
Cale Teeter <cale.teeter@microsoft.com>
2023-10-20 12:02:09 -0400

    Update to add exception for missing connection string

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -26,6 +26,9 @@ export class Connection {
 
   constructor(connectionString: string, creds?: string[], tls?: boolean) {
     const params = connectionString.split(":");
+    if (!params) {
+      throw new Error("Missing or invalid connection string");
+    }
 
     const options: nodeq.ConnectionParameters = {
       nanos2date: false,
