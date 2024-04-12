20a6b6c67a2353dbcd8388d3301f82bc4c5a5eb5
Philip Carneiro <pcarneiro@kx.com>
2024-04-12 12:06:55 +0100

    remove connections

--- src/classes/localConnection.ts
+++ src/classes/localConnection.ts
@@ -10,7 +10,6 @@
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-/* istanbul ignore file */
 
 import * as nodeq from "node-q";
 import { window } from "vscode";
