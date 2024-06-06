2abf02eae179ea4ead939043feb529d265f47171
Philip Carneiro <pcarneiro@kx.com>
2024-06-06 16:14:19 +0100

    fix local conn issues

--- src/classes/localConnection.ts
+++ src/classes/localConnection.ts
@@ -12,7 +12,7 @@
  */
 
 import * as nodeq from "node-q";
-import { window } from "vscode";
+import { commands, window } from "vscode";
 import { ext } from "../extensionVariables";
 import { delay, kdbOutputLog } from "../utils/core";
 import { convertStringToArray, handleQueryResults } from "../utils/execution";
@@ -86,11 +86,11 @@ export class LocalConnection {
         return;
       }
       conn.addListener("close", () => {
-        kdbOutputLog(
-          `Connection closed: ${this.options.host}:${this.options.port}`,
-          "INFO",
+        commands.executeCommand("kdb.disconnect", this.connLabel);
+        ext.outputChannel.appendLine(
+          `Connection stopped from ${this.options.host}:${this.options.port}`,
         );
-        this.connected = false;
+        ext.outputChannel.show();
       });
 
       if (this.connection && this.connected) {
