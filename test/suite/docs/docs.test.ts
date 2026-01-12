175dbf8bffd212f4a1237b1b166f6f823779df08
Niall Farrell <nfarrell@kx.com>
2026-01-12 17:29:34 +0000

    Update docs.test.ts

--- test/suite/docs/docs.test.ts
+++ test/suite/docs/docs.test.ts
@@ -12,19 +12,17 @@
  */
 
 import * as assert from "assert";
-import { readdirSync, readFileSync } from "node:fs";
+import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 
 describe("Docs", () => {
   let readme: string;
-  let images: string[];
 
   before(() => {
     const root = resolve(__dirname, "..", "..", "..", "..");
     readme = readFileSync(resolve(root, "README.md"), {
       encoding: "utf8",
     });
-    images = readdirSync(resolve(root, ".README"));
   });
 
   describe("README", () => {
@@ -36,26 +34,6 @@ describe("Docs", () => {
           assert.ok(match[1].startsWith("https://"));
         }
       }
-    });
-    it("should contain all images", () => {
-      for (const img of images) {
-        const index = readme.indexOf(`/${img}`);
-        assert.ok(index !== -1);
-      }
-    });
-    it("should have valid links to images", () => {
-      const regex = /\(https:\/\/([^)]*)\/.README\/([^)]*)\)/gs;
-      const linked = new Set();
-      let match: RegExpExecArray;
-      while ((match = regex.exec(readme))) {
-        assert.strictEqual(
-          match[1],
-          "raw.githubusercontent.com/KxSystems/kx-vscode/main",
-        );
-        assert.ok(images.includes(match[2]));
-        linked.add(match[2]);
-      }
-      assert.strictEqual(linked.size, images.length);
     });
   });
 });
